7abcd7312c18b80aa3ac3807d6f532a9db5357d1
Dale Roberts <dale.o.roberts@gmail.com>
2013-05-23 11:59:37 +1000

    Cleanup

--- src/exact.h
+++
@@ -1,8 +0,0 @@
-#ifndef _volmodels_exact
-#define _volmodels_exact
-
-#include <Rcpp.h>
-
-RcppExport SEXP pintsqr_c(SEXP kappa_, SEXP theta_, SEXP sigma_, SEXP u_, SEXP vu_, SEXP t_, SEXP vt_, SEXP x_);
-
-#endif
